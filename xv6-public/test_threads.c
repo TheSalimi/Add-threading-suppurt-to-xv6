a2d45159d8e45ba6fd7ec69aa91d6ffa8a98f52f
Amir Salimi <amirhosein.srudsari@gmail.com>
2024-01-30 19:07:14 +0330

    add test

--- xv6-public/test_threads.c
+++ xv6-public/test_threads.c
@@ -1,70 +1,64 @@
 #include "types.h"
-#include "param.h"
-#include "memlayout.h"
-#include "mmu.h"
-#include "proc.h"
 #include "user.h"
-#include "stat.h"
-#include "fcntl.h"
 #include "x86.h"
 
-#define SLEEP_TIME 100
+#define N 3
+#define DELAY_MS 100
+lock_t lock;
 
-volatile int counter = 0;
+int matrix_a[N][N] = {
+    {1, 2, 3},
+    {4, 5, 6},
+    {7, 8, 9}
+};
 
-lock_t* lock;
+int matrix_b[N][N] = {
+    {9, 8, 7},
+    {6, 5, 4},
+    {3, 2, 1}
+};
 
-void thread1_func(void* arg1, void* arg2) 
-{
-  int num = *(int*)arg1;
-  if (num) lock_acquire(lock);
-  {
-    counter++;
-    printf(1, "1. First thread : %d \n", counter );
-  }  
-  sleep(SLEEP_TIME);
-  if (num) lock_release(lock);
-  exit();
+int result[N][N];
+
+void multiply_row(void *arg1, void *arg2) {
+    int row = *(int*)arg1;
+    int i, j;
+    lock_acquire(&lock);
+    printf(1, "Thread for row %d started.\n", row );
+    lock_release(&lock);
+
+    for (i = 0; i < N; i++) {
+        result[row][i] = 0;
+        for (j = 0; j < N; j++) {
+            result[row][i] += matrix_a[row][j] * matrix_b[j][i];
+        }
+    }
+    lock_acquire(&lock);
+    printf(1, "Thread for row %d finished.\n",row);
+    lock_release(&lock);
+    exit();
 }
 
-void thread2_func(void* arg1, void* arg2) 
-{
-  int num = *(int*)arg1;
-  if (num) lock_acquire(lock);
-  {
-    counter++;
-    printf(1, "2. Second Thread: %d \n", counter );
-  }
-  sleep(SLEEP_TIME);
-  if (num) lock_release(lock);
-  exit();
-}
+int main() {
+    lock_init(&lock);
+    int *row;
+    for (int i = 0; i < N; i++) {
+        row = malloc(sizeof(int)); // Allocate memory for row
+        *row = i; // Set the value of row
+        thread_create(multiply_row, row, 0); // Pass the address of row
+    }
+    for (int i = 0; i < N; i++) {
+        thread_join();
+    }
 
-void thread3_func(void* arg1, void* arg2) 
-{
-  int num = *(int*)arg1;
-  if (num) lock_acquire(lock);
-  {
-    counter++;
-    printf(1, "3. Third Thread: %d \n", counter );
-  }
-  sleep(SLEEP_TIME);
-  if (num) lock_release(lock);
-  exit();
-}
+    // Print the result
+    printf(1, "Matrix multiplication result:\n");
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf(1, "%d ", result[i][j]);
+        }
+        printf(1, "\n");
+    }
 
-int
-main(int argc, char *argv[])
-{
-  lock_init(lock);
-  int arg1 = 1, arg2 = 1;
-
-  thread_create(&thread1_func, (void *)&arg1, (void *)&arg2);
-  thread_create(&thread2_func, (void *)&arg1, (void *)&arg2);
-  thread_create(&thread3_func, (void *)&arg1, (void *)&arg2);
-  thread_join();
-  thread_join();
-  thread_join();
-  
-  exit();
+    exit();
 }
